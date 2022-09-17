ca2a3e49891c5441b65c61b7a024592647673566
Wendy Rivero <60824265+RiveroWendy@users.noreply.github.com>
2022-09-17 14:35:10 -0300

    project files

---
+++ build/precache-manifest.f422a21fcdbeeb372011d8506e351916.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2977bb1edd84faa1cbdd53b069de4c1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f95150856726b2ee763",
+    "url": "/static/css/main.708e877a.chunk.css"
+  },
+  {
+    "revision": "87e13c6374bb48ef2f03",
+    "url": "/static/js/2.c12b4316.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.c12b4316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f95150856726b2ee763",
+    "url": "/static/js/main.b63ca816.chunk.js"
+  },
+  {
+    "revision": "dab11903091cda04344c",
+    "url": "/static/js/runtime-main.71f22666.js"
+  },
+  {
+    "revision": "427a4adc3c0a0dec192d14617aaa6c4c",
+    "url": "/static/media/logo.427a4adc.svg"
+  }
+]);
